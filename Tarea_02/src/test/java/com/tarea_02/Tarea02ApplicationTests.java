856f845d9abb96e1e5880c6154fdeb114c12bac5
pedru <pedru@MSI>
2022-02-16 17:25:15 -0600

    Primer Commit de la Tarea_02

---
+++ Tarea_02/src/test/java/com/tarea_02/Tarea02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
