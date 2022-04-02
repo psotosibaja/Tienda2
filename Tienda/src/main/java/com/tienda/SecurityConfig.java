37f2a3561b60e4ae24cb85a2fb6b34e99b7a7a56
pedru <pedru@MSI>
2022-04-02 16:23:41 -0600

    Semana 10

--- Tienda/src/main/java/com/tienda/SecurityConfig.java
+++ Tienda/src/main/java/com/tienda/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @Configuration //archivos de configuracion
 @EnableWebSecurity //advierte que esta clase tiene que ver son la seguridad
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
